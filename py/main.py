e602004282b73c4ea28f0f852f84ffc038744fe6
Bibby <bibby.lai@asmpt.com>
2024-09-27 17:49:06 +0800

    update

--- py/main.py
+++ py/main.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 # 設定參數
 fs = 44100  # 取樣率
-duration = 1.5  # 每次錄音的時長 (秒)
-noise_threshold = 0.1  # 噪音閾值，越小越敏感
+duration = 1  # 每次錄音的時長 (秒)
+noise_threshold = 0.015  # 噪音閾值，越小越敏感
 save_folder = "audiolog"  # 儲存錄音片段的資料夾
 
 # 如果資料夾不存在，創建它
@@ -17,11 +17,12 @@ if not os.path.exists(save_folder):
 def record_and_save_noise():
     """ 持續錄音並保存噪音片段 """
     print("開始持續錄音，檢測噪音...")
-    
+
+    print(sd.query_devices())
     try:
         while True:
             # 錄音 1 秒的音頻數據
-            audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+            audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1, device=1)
             sd.wait()  # 等待錄音完成
             
             # 計算振幅，並檢測是否超過噪音閾值
@@ -31,7 +32,7 @@ def record_and_save_noise():
             if max_amplitude > noise_threshold:
                 # 如果超過閾值，保存音頻
                 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                filename = os.path.join(save_folder, f"noise_{timestamp}.wav")
+                filename = os.path.join(save_folder, f"noise_{timestamp}-{max_amplitude}.wav")
                 write(filename, fs, audio_data)
                 print(f"噪音檢測到，保存片段: {filename}")
     
